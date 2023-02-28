49ebc09374851077decd2095f37db4160024db2c
Elena <ebromeo88@gmail.com>
2023-02-28 19:29:33 +0100

    Add app.ts file

---
+++ src/app.ts
@@ -0,0 +1,63 @@
+import express, { NextFunction, Request, Response } from 'express';
+import morgan from 'morgan';
+import cors from 'cors';
+import { thingsRouter } from './routers/things.router.js';
+import createDebug from 'debug';
+import { CustomError } from './interfaces/interfaces.js';
+
+const debug = createDebug('w6:app');
+
+export const app = express();
+app.disable('x-powered-by');
+
+const corsOptions = {
+  origin: '*',
+};
+app.use(morgan('dev'));
+app.use(express.json());
+app.use(cors(corsOptions));
+app.use(express.static('public'));
+
+app.use((_req, _resp, next) => {
+  debug('Soy un middleware');
+  next();
+});
+
+app.use('/things', thingsRouter);
+
+app.get('/', (_req, resp) => {
+  resp.json({
+    info: '/Esta es una prueba',
+    endpoints: {
+      things: '/things',
+    },
+  });
+});
+app.get('/:id', (req, resp) => {
+  resp.send('Hola ' + req.params.id);
+});
+app.post('/', (req, resp) => {
+  req.body.id = 12;
+  resp.send(req.body);
+});
+app.patch('/:id');
+app.delete('/:id');
+
+app.use(
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  (error: CustomError, _req: Request, resp: Response, _next: NextFunction) => {
+    debug('Soy el middleware de errores');
+    const status = error.statusCode || 500;
+    const statusMessage = error.statusMessage || 'Internal server error';
+    resp.status(status);
+    resp.json({
+      error: [
+        {
+          status,
+          statusMessage,
+        },
+      ],
+    });
+    debug(status, statusMessage, error.message);
+  }
+);
